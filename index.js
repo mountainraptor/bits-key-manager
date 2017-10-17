8ec768d68f9943a66281659d4855895f8c4a73c0
Adam Markham <1291252+mountainraptor@users.noreply.github.com>
2017-10-17 14:05:46 -0600

    feat: work with existing security scopes
    Create new messenger calls for module
    Remove calls to KeyMessenger with KeysApi scope

--- index.js
+++ index.js
@@ -5,19 +5,23 @@
 (() => {
   'use strict';
 
+  const path = require('path');
   const KeysManager = require('./lib/keys-manager');
   const BaseServer = global.helper.BaseServer;
+  const KeyManager = require(path.resolve(global.paths.data, '../lib/key/key-manager'));
 
   class KeysManagerApp {
     constructor() {
       this._baseServer = new BaseServer({logging: true});
-      this._keysManager = new KeysManager();
+      this._keyManager = new KeyManager();
+      this._keysManager = new KeysManager(this._keyManager);
     }
 
     load(messageCenter) {
       return Promise.resolve()
       .then(() => this._baseServer.load(messageCenter))
-      .then(() => this._keysManager.load(messageCenter, this._baseServer));
+      .then(() => this._keyManager.load(messageCenter))
+      .then(() => this._keysManager.load(messageCenter, this._baseServer, this._keyManager));
     }
 
     unload(messageCenter) {
